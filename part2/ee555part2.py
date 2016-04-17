3b330155ddc1a789fec71209462419747b1efacc
Hao Zhang <zhan849@usc.edu>
2016-04-17 16:59:05 -0700

    added part 2 files

---
+++ part2/ee555part2.py
@@ -0,0 +1,42 @@
+"""Custom topology example
+
+    EE 555 Project - Router Exercise - Part 2
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+
+class ee555P2( Topo ):
+
+    def __init__( self ):
+        "Create custom topo."
+        # Initialize topology
+        Topo.__init__( self )
+
+        self._hosts = []
+        self._switches = []
+        self._links = []
+
+        self._hosts.append( self.addHost('h1', ip='10.0.1.2/24', defaultRoute = 'via 10.0.1.1') )
+        self._hosts.append( self.addHost('h2', ip='10.0.1.3/24', defaultRoute = 'via 10.0.1.1') )
+        self._hosts.append( self.addHost('h3', ip='10.0.2.2/24', defaultRoute = 'via 10.0.2.1') )
+        self._hosts.append( self.addHost('h4', ip='10.0.2.3/24', defaultRoute = 'via 10.0.2.1') )
+        self._hosts.append( self.addHost('h5', ip='10.0.2.4/24', defaultRoute = 'via 10.0.2.1') )
+
+        self._switches.append( self.addSwitch('s1') )
+        self._switches.append( self.addSwitch('s2') )
+
+        self._links.append( self.addLink( 's1', 's2', port1=1, port2=1 ) )
+        self._links.append( self.addLink( 'h1', 's1', port1=1, port2=2 ) )
+        self._links.append( self.addLink( 'h2', 's1', port1=1, port2=3 ) )
+        self._links.append( self.addLink( 'h3', 's2', port1=1, port2=2 ) )
+        self._links.append( self.addLink( 'h4', 's2', port1=1, port2=3 ) )
+        self._links.append( self.addLink( 'h5', 's2', port1=1, port2=4 ) )
+
+    @classmethod
+    def create(cls): return cls()
+
+topos = { 'ee555P2': ee555P2.create }
+
